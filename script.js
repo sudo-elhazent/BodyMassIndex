d382cab7ce2f1d34baf9f4ef54457dd09bef21f4
ALFIAN SATRIA V.P <masfian77@gmail.com>
2019-10-09 06:37:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function bmi(beratBadan, tinggiBadan) {
-	var rumus = beratBadan/(tinggiBadan/100*tinggiBadan/100);
-	console.log(rumus);
-  if (rumus < 17.0){
+function bmi(beratTubuh, tinggiTubuh) {
+	var rumus = beratTubuh/(tinggiTubuh/100*tinggiTubuh/100);
+	console.log(rumusnya);
+  if (rumusnya < 17.0){
 		return "Kurus, Kekurangan berat badan berat";
-	} else if (rumus >= 17.0 && rumus <= 18.4){
+	} else if (rumusnya >= 17.0 && rumusnya <= 18.4){
 		return "Kurus, Kekurangan berat badan ringan";
-	} else if (rumus >= 18.5 && rumus <= 27.0){
+	} else if (rumusnya >= 18.5 && rumusnya <= 27.0){
 		return  "Normal";
-	} else if (rumus >= 25.1 && rumus <= 27.0){
+	} else if (rumusnya >= 25.1 && rumusnya <= 27.0){
 		return "Gemuk, Kelebihan berat badan tingkat ringan";
-	} else if (rumus > 27.0){
-		return"Gemuk, Kelebihan berat badan tingkat berat";
+	} else if (rumusnya > 27.0){
+		return"Gemuk sekali, Kelebihan berat badan tingkat berat";
 	}
 }
 	
